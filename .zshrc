43d53be18611e5693495cc6502e09ef77dd3e74e
jmaeso <jmaeso@enti.cat>
2021-06-03 11:07:20 +0200

    Small cleanup

--- .zshrc
+++ .zshrc
@@ -65,8 +65,8 @@ PROMPT=$PROMPT'$(kube_ps1) '
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/paack/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/paack/google-cloud-sdk/path.zsh.inc'; fi
+[ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
 
-# The next line enables shell command completion for gcloud.
-if [ -f '/Users/paack/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/paack/google-cloud-sdk/completion.zsh.inc'; fi
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+source ~/.env
--- setup.macos.sh
+++ setup.macos.sh
@@ -1,14 +1,7 @@
-# bash configs
-ln -s ~/Code/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/Code/dotfiles/.bash_custom ~/.bash_custom
+# basic configs
 ln -s ~/Code/dotfiles/.env ~/.env
 ln -s ~/Code/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/Code/dotfiles/.gitignore_global ~/.gitignore_global
-
-# 3rd party configs
-ln -s ~/Code/dotfiles/.aws ~/.aws
-ln -s ~/Code/dotfiles/.docker ~/.docker
-ln -s ~/Code/dotfiles/.kube ~/.kube
 
 # vim config
 ln -s ~/Code/dotfiles/vim/.vimrc ~/.vimrc
@@ -17,6 +10,3 @@ ln -s ~/Code/dotfiles/vim/colors ~/.vim/colors
 # vs code config
 ln -s ~/Code/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 ln -s ~/Code/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-
-# for the record
-# git branch autocompletion - brew install bash-completion
